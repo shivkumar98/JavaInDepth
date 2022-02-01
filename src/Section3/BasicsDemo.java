02312343d7d5f9da0692bbc191bad99ef2423039
unknown <shiv_kumar.98@outlook.com>
2022-02-01 22:05:22 +0000

    first commit#

---
+++ src/Section3/BasicsDemo.java
@@ -0,0 +1,17 @@
+package Section3;
+
+import java.nio.channels.FileLockInterruptionException;
+
+public class BasicsDemo {
+	
+	public static void main(String[] args) {
+		int i = 6;
+		int j = 2;
+		System.out.println(i+j); // 8
+		System.out.println(i-j); // 4
+		System.out.println(i*j); // 12
+		System.out.println(i/j); // 3
+		System.out.println(i%j); // 0
+	}
+
+}
